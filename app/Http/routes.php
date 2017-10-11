c66379db9e232385c31fea1b3ff744195cfa6ae1
s3a317097 <31909582+s3a317097@users.noreply.github.com>
2017-10-11 21:26:45 +0800

    2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/',['as'=>'home.index','uses'=>'HomeController@index']);
+Route::get('/about',['as'=>'about.index','uses'=>'AboutController@index']);
+Route::get('/news',['as'=>'news.index','uses'=>'NewsController@index']);
